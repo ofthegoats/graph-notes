f88430d6007dcafb975d5860b9e4dfa17061d366
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2022-02-01 11:50:24 +0000

    Tests for linked lists

---
+++ tests/linked_list_test.c
@@ -0,0 +1,104 @@
+#include "../linked_list.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void traverse(struct linked_list* ll);
+
+void traverse_test();
+void append_test();
+void pop_test();
+void search_test();
+
+int main(int argc, char* argv[])
+{
+    append_test();
+    traverse_test();
+    pop_test();
+    search_test();
+    return 0;
+}
+
+void traverse(struct linked_list* ll)
+{
+    struct node* current = ll->head;
+    while (current) {
+        printf("%p : %p\n", current, current->val);
+        current = current->next;
+    }
+}
+
+void traverse_test()
+{
+    struct linked_list ll = {NULL};
+    append(&ll, (void*)1);
+    append(&ll, (void*)2);
+    append(&ll, (void*)3);
+    append(&ll, (void*)4);
+    traverse(&ll);
+}
+
+void append_test()
+{
+    printf("%s: ", __FUNCTION__);
+    struct linked_list ll = {NULL};
+    append(&ll, (void*)1);
+    append(&ll, (void*)2);
+    append(&ll, (void*)3);
+    append(&ll, (void*)4);
+    assert(ll.head->val == (void*)4);
+    assert(ll.head->next->val == (void*)3);
+    assert(ll.head->next->next->val == (void*)2);
+    assert(ll.head->next->next->next->val == (void*)1);
+    printf("PASSED\n");
+}
+
+void pop_test()
+{
+    printf("%s: ", __FUNCTION__);
+    struct linked_list ll = {NULL};
+    struct node* one = append(&ll, (void*)1);
+    struct node* two = append(&ll, (void*)2);
+    struct node* three = append(&ll, (void*)3);
+    struct node* four = append(&ll, (void*)4);
+    struct node* five = append(&ll, (void*)5);
+    struct node* six = append(&ll, (void*)6);
+    pop(&ll, (void*)1); // remove from end
+    assert(ll.head == six);
+    assert(ll.head->next == five);
+    assert(ll.head->next->next == four);
+    assert(ll.head->next->next->next == three);
+    assert(ll.head->next->next->next->next == two);
+    assert(ll.head->next->next->next->next->next == NULL);
+    pop(&ll, (void*)6); // remove from head
+    assert(ll.head == five);
+    assert(ll.head->next == four);
+    assert(ll.head->next->next == three);
+    assert(ll.head->next->next->next == two);
+    pop(&ll, (void*)3); // remove from middle
+    assert(ll.head == five);
+    assert(ll.head->next == four);
+    assert(ll.head->next->next == two);
+    pop(&ll, (void*)11); // remove non-existent
+    assert(ll.head == five);
+    assert(ll.head->next == four);
+    assert(ll.head->next->next == two);
+    printf("PASSED\n");
+}
+
+void search_test()
+{
+    printf("%s: ", __FUNCTION__);
+    struct linked_list ll = {NULL};
+    struct node* one = append(&ll, (void*)1);
+    struct node* two = append(&ll, (void*)2);
+    struct node* three = append(&ll, (void*)3);
+    struct node* four = append(&ll, (void*)4);
+    assert(search(&ll, (void*)1) == one);
+    assert(search(&ll, (void*)2) == two);
+    assert(search(&ll, (void*)3) == three);
+    assert(search(&ll, (void*)4) == four);
+    assert(search(&ll, (void*)5) == NULL);
+    printf("PASSED\n");
+}
